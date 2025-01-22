c70f2edd21cd14bda2e938be716fc2d92405465b
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2025-01-22 15:56:41 -0600

    update

---
+++ Unidad 1/P03_CajaDeTexto_LineEdit.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5 import uic, QtWidgets
+qtCreatorFile = "P03_CajaDeTexto_LineEdit.ui"  # Nombre del archivo aquí.
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        # Área de los Signals
+        self.btn_saludar.clicked.connect(self.saludar)
+
+    # Área de los Slots
+    def saludar(self):
+        nombre = self.txt_nombre.text()
+        #print("Hola!")
+        self.msj("Hola " + nombre)
+
+    def msj(self, txt):
+        m = QtWidgets.QMessageBox()
+        m.setText(txt)
+        m.exec_()
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
+
